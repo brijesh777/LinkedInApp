6c126f8c08e7fb16d81ef963ad2d70e6c6c1b54c
brijesh777 <makwanabrijesh777@gmail.com>
2026-04-26 12:38:01 -0400

    Update Properties files

--- posts-service/src/main/java/com/codingshuttle/linkedin/posts_service/controller/PostsController.java
+++ posts-service/src/main/java/com/codingshuttle/linkedin/posts_service/controller/PostsController.java
@@ -7,18 +7,34 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/posts")
 @RequiredArgsConstructor
 public class PostsController {
 
     private final PostsService postsService;
-    
+
     @PostMapping
     public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto, Long userId) {
         PostDto createdPost = postsService.createdPost(postDto, userId);
-        PostDto createPost =postsService.createdPost(postDto, 1L);
+        PostDto createPost = postsService.createdPost(postDto, 1L);
         return new ResponseEntity<>(createdPost, HttpStatus.CREATED);
 
     }
+
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<PostDto> getPost(@PathVariable Long postId) {
+        PostDto postDto = postsService.getPostById(postId);
+        return ResponseEntity.ok(postDto);
+    }
+
+    @GetMapping("/users/{userId}/allPosts")
+    public ResponseEntity<List<PostDto>> getAllPostsOfUser(@PathVariable Long userId) {
+        List<PostDto> posts = postsService.getAllPostsOfUser(userId);
+        return ResponseEntity.ok(posts);
+    }
+
 }

5e972ef1450eea993ee797ac8144d12ba682e414
brijesh777 <makwanabrijesh777@gmail.com>
2026-04-26 12:25:53 -0400

    Added Like/Unlike Post service feature and global exception handling

--- posts-service/src/main/java/com/codingshuttle/linkedin/posts_service/exception/ApiError.java
+++ posts-service/src/main/java/com/codingshuttle/linkedin/posts_service/exception/ApiError.java
@@ -8,4 +8,16 @@ import java.time.LocalDateTime;
 @Data
 public class ApiError {
 
+    private LocalDateTime timestamp;
+    private String error;
+    private HttpStatus statusCode;
+
+    public ApiError() {
+        this.timestamp = LocalDateTime.now();
+    }
+    public ApiError(String error, HttpStatus statusCode) {
+        this();
+        this.error = error;
+        this.statusCode = statusCode;
+    }
 }

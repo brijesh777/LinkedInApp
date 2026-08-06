30e0d02385449695dc794bbd8d8d0d376a99ee5a
brijesh777 <makwanabrijesh777@gmail.com>
2026-08-05 21:34:27 -0400

    Delete build_images.sh

--- build_images.sh
+++
@@ -1,6 +0,0 @@
-docker build -t farooque85/linkedin-app-api-gateway ./api-gateway
-docker build -t farooque85/linkedin-app-connections-service ./connections-service
-docker build -t farooque85/linkedin-app-discovery-server ./discovery-server
-docker build -t farooque85/linkedin-app-notification-service ./notification-service
-docker build -t farooque85/linkedin-app-posts-service ./posts-service
-docker build -t farooque85/linkedin-app-user-service ./user-service
